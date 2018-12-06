75bfd54dd3f50fc816b641cf80abdb3587495b60
TheRedeemed <abdoulkaders77@gmail.com>
2018-12-05 22:50:23 -0500

    Added Project properties and ProjectRepository
    Project class fields, constructor, getters and setters ProjectRepository

---
+++ PPM/src/main/java/com/theredeemed/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.theredeemed.ppmtool.repository;
+
+import com.theredeemed.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
