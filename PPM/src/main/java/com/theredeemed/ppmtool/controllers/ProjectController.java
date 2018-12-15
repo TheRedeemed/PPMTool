11ab47fa1fe7ccbb9aef481890a5676cc682a005
TheRedeemed <abdoulkaders77@gmail.com>
2018-12-14 23:19:31 -0500

    Validation - Custom returned message
    Returning a custom error message when there errors.
    returning a json with object property and error message

--- PPM/src/main/java/com/theredeemed/ppmtool/controllers/ProjectController.java
+++ PPM/src/main/java/com/theredeemed/ppmtool/controllers/ProjectController.java
@@ -6,9 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,7 +26,11 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError fieldError: result.getFieldErrors()) {
+                errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<Project>(projectService.saveOrUpdateProject(project), HttpStatus.CREATED);
     }
